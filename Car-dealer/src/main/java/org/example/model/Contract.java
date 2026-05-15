af5d513adf63fe034bb81d280efbfd8ec2837104
sebly A <sebliassefa6@gmail.com>
2026-05-15 13:59:33 -0700

    contract class

---
+++ Car-dealer/src/main/java/org/example/model/Contract.java
@@ -0,0 +1,54 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public abstract class Contract {
+    private String contractDate;
+    private String customerName;
+    private String customerEmail;
+    private boolean isVehicleSold;
+    private Vehicle theVehicle;
+    private double totalPrice;
+    private double monthPayment;
+    public Contract(LocalDate contractDate,String customerName,String customerEmail,Vehicle vehicleSold) {
+    }
+    public String getContractDate() {
+        return contractDate;
+    }
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public boolean isVehicleSold() {
+        return isVehicleSold;
+    }
+
+    public void setVehicleSold(boolean vehicleSold) {
+        isVehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalprice();
+
+    public abstract double setTotalprice();
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+}
+
